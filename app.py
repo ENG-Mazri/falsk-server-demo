95e0cd29792d8c8e7923ccd68109d2f82f73904f
Abderrahmane <amazri@tangl.cloud>
2024-09-23 16:49:02 +0100

    add cors

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from blueprints.demo.demo import demo_bp
 from blueprints.ifc.ifc import ifc_bp
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(demo_bp)
 app.register_blueprint(ifc_bp)
 
